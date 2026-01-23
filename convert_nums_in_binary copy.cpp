bcec71ab8109b48246544d0fa3481e812f8d4371
tr285 <goretukaram62@gmail.com>
2026-01-23 21:52:01 +0530

    update code

--- convert_nums_in_binary copy.cpp
+++ convert_nums_in_binary copy.cpp
@@ -3,16 +3,16 @@ using namespace std;
 int main()
 {
     int x;
-    int bi[32];
+    int bi[8];
     cin >> x;
 
-    for (int i = 0; i < 32; i++)
+    for (int i = 0; i < 8; i++)
     {
-        int current = (31 - i);
+        int current = (7 - i);
         bi[current] = (x % 2);
         x /= 2;
     }
-    for (int j = 0; j < 32; j++)
+    for (int j = 0; j < 8; j++)
     {
         cout << bi[j];
     }
