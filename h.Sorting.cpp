d61c98d29be8ed82af7d4029b345d2e231b5884a
Tukaram Gore <goretukaram62@gmail.com>
2026-02-14 20:38:02 +0530

    Add files via upload
    to sort an array

---
+++ h.Sorting.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int A[N];
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    // Bubble Sort
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - i - 1; j++) {
+            if (A[j] > A[j + 1]) {
+                int temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        cout << A[i] << " ";
+    }
+
+    return 0;
+}
