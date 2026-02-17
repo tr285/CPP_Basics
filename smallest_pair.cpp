b4bb3a134a3b5185129d328c4a980af6aafbcc97
Tukaram Gore <goretukaram62@gmail.com>
2026-02-17 22:46:45 +0530

    Add files via upload
    smallest pair finding

---
+++ smallest_pair.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+
+        vector<long long> A(N);
+        for (int i = 0; i < N; i++) {
+            cin >> A[i];
+        }
+
+        long long minSoFar = A[0] - 1;   // A[i] - i (1-based)
+        long long ans = LLONG_MAX;
+
+        for (int j = 1; j < N; j++) {
+            ans = min(ans, minSoFar + A[j] + (j + 1));
+            minSoFar = min(minSoFar, A[j] - (j + 1));
+        }
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
