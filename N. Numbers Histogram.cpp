f5e872ecf910a36f620705446621dcbe863021be
Tukaram Gore <goretukaram62@gmail.com>
2026-03-12 22:09:32 +0530

    Add files via upload
    this code print an string keyword in n numbers of time using loop

---
+++ N. Numbers Histogram.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+int main (){
+    char S;
+    int N;
+    cin >>S;
+    cin >>N;
+    for (int i=0;i<N;i++){
+        int X;
+        cin >> X;
+        for (int j=0; j<X; j++){
+            cout << S;
+        }
+        cout <<endl;
+
+    }
+    return 0;
+}
